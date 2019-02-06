7649d644a9ddd94505b733a18369bdf13a982098
RNDM-IMPLOSIONS <La129Guna>
2019-02-06 15:44:01 +0100

    Generic message

--- Dumb-Shit/Program.cs
+++ Dumb-Shit/Program.cs
@@ -6,7 +6,23 @@ namespace Dumb_Shit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How are you?");
+            string userStatus = Console.ReadLine();
+            switch (userStatus.ToLower())
+            {
+                case "good":
+                    Console.WriteLine("Great I'm glad you're good.");
+                    break;
+                case "bad":
+                    Console.WriteLine("Oh, I'm sorry to hear that.");
+                    break;
+                case "":
+                    Console.WriteLine("Fine then, keep your secrets.");
+                    break;
+                default:
+                    Console.WriteLine("Alright.");
+                    break;
+            }
         }
     }
 }
